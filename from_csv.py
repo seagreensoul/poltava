5241fd70ca0c5a38dd44f4fe68a5a5e0efa94af3
seagreensoul <smailik1107@ya.ru>
2018-05-18 11:43:34 +0300

    Update from_csv.py

--- from_csv.py
+++ from_csv.py
@@ -1,22 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr  5 13:26:26 2018
-
-@author: admin
-"""
-
 import pandas as pd
 import geocoder 
-#import folium
 
-
-
-df= pd.read_csv('poltava.csv',
-                       sep=';', encoding='cp1251')
-
+df= pd.read_csv('poltava.csv',sep=';', encoding='cp1251')
 f = open('poltava_geo1.csv', 'w')
 f.write("\"lat\";\"lng\"\n")
-
 
 for adr in df['adress']:
     adr = "Полтавська область "+adr
@@ -27,6 +14,4 @@ for adr in df['adress']:
         f.write(l[0]+";"+l[1]+"\n")
     except TypeError:
         continue
-
-
 f.close()
